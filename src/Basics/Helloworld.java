29deea2cab2cecaa533aff99c6e950d9dabe6a3e
Khalil-Chrayah <56394297+Khalil-Chrayah@users.noreply.github.com>
2019-10-11 08:36:02 +0100

    Update Helloworld.java

--- src/Basics/Helloworld.java
+++ src/Basics/Helloworld.java
@@ -5,5 +5,6 @@ public class Helloworld {
    public static void main(String[] args) {
 	   System.out.println("Hello world");
 	   System.out.println("i'm adding code to my project");
+	   System.out.println("adding code from GitHube");
    }
 }
